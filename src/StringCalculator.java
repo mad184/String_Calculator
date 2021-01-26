274f49e18c5fd6cf6f46cd6a4b0f03009be84183
mad184 <mad184@usask.ca>
2021-01-25 20:53:30 -0600

    Add a main to print the results for a better mapping.

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -54,6 +54,27 @@ public class StringCalculator {
         if (add(numberTest_5) != 0){
             System.out.println("The expected result was 0, instead it was "+ add(numberTest_5));
         }
+    }
 
+    public static void main(String[] args) {
+
+        String numberTest_1 = "1,2,5";
+        String numberTest_2 = "1,1,1";
+        String numberTest_3 = "0,75,5";
+        String numberTest_4 = "";
+        String numberTest_5 = " ";
+
+        int result1 = add(numberTest_1);
+        int result2 = add(numberTest_2);
+        int result3 = add(numberTest_3);
+        int result4 = add(numberTest_4);
+        int result5 = add(numberTest_5);
+
+        System.out.println("The outputs from each entries are: \n" +
+                "Sum for string numberTest_1 = " + result1 +
+                "\nSum for string numberTest_2 = " + result2 +
+                "\nSum for string numberTest_3 = " + result3 +
+                "\nSum for string numberTest_4 = " + result4 +
+                "\nSum for string numberTest_5 = " + result5);
     }
 }
