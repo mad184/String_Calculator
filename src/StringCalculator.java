73dda783c8da6fdb14416691441c568a7158228f
mad184 <mad184@usask.ca>
2021-01-26 15:58:48 -0600

    New method differentDelimiters() to so string can have multiple delimiter. - now the add() method can satisfies all the requirements + the Bonus .

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 
 
 public class StringCalculator {
+
+    static String[] temporaryStorage;
 
     /**
      * A string calculator that receive as input a String with numbers separated
@@ -32,17 +36,24 @@ public class StringCalculator {
 
 
             if (delimiter.length() > 1){
-                splitString = halfSplit[1].split("[" + delimiter + "]");
 
-                String buildString = "";
-                for (String t: splitString){
-                    if (t.isBlank()){
-                        buildString+=",";
-                    }else {
-                        buildString += t;
+                if (delimiter.contains(",")){
+                    temporaryStorage = new String[0];
+                    splitString = differentDelimiters(delimiter, halfSplit[1]);
+                }else {
+
+                    splitString = halfSplit[1].split("[" + delimiter + "]");
+
+                    String buildString = "";
+                    for (String t : splitString) {
+                        if (t.isBlank()) {
+                            buildString += ",";
+                        } else {
+                            buildString += t;
+                        }
                     }
+                    splitString = buildString.split(",".repeat(delimiter.length()-1));
                 }
-                splitString = buildString.split(",".repeat(delimiter.length()-1));
             }else {
                 splitString = halfSplit[1].split("[" + delimiter + "]");
             }
@@ -50,6 +61,7 @@ public class StringCalculator {
             int totalSum = 0;
 
             for (String s : splitString) {
+
                 if (s.isBlank()){
                     return 0;
 
@@ -64,15 +76,16 @@ public class StringCalculator {
                 } else if(s.length()==4) {
                     if (Integer.parseInt(s) < 0){
                         throw new Exception("Negatives not allowed, number " + s + " is negative, therefore not allowed");
-                    }
+                    }else {
 
-                    int firstNumber = Integer.parseInt(String.valueOf(s.charAt(0)));
-                    int lastNumber = Integer.parseInt(String.valueOf(s.charAt(s.length()-1)));
+                        int firstNumber = Integer.parseInt(String.valueOf(s.charAt(0)));
+                        int lastNumber = Integer.parseInt(String.valueOf(s.charAt(s.length() - 1)));
 
-                    if (firstNumber == 1 && lastNumber == 0){
-                        totalSum += Integer.parseInt(s);
-                    }else if (firstNumber == 0){
-                        totalSum += Integer.parseInt(s);
+                        if (firstNumber == 1 && lastNumber == 0) {
+                            totalSum += Integer.parseInt(s);
+                        } else if (firstNumber == 0) {
+                            totalSum += Integer.parseInt(s);
+                        }
                     }
 
                 } else if (s.length() < 4){
@@ -87,20 +100,40 @@ public class StringCalculator {
         }
     }
 
+    public static  String[] differentDelimiters(String delimitersString, String numberString){
+        String [] resultString;
+        String[] delimitersArray = delimitersString.split(",");
+
+        for (String aDelimiter:delimitersArray){
+
+            if (numberString.contains(aDelimiter)){
+
+                numberString = numberString.replace(aDelimiter, "!");
+
+            }
+        }
+        numberString = numberString.replaceAll("[!]+","!");
+
+        resultString = numberString.split("!");
+
+        return resultString;
+    }
+
     /**
      * A test that compares the values expected with the actual value return by the method add()
      */
     @Test
     public void rightSumTest() throws Exception {
-        String numberTest_1 = "//@@\n1@@1001@@0";
-        String numberTest_2 = "//@\n2@3@009998";
+        String numberTest_1 = "//**,#,!\n1**1001#0!2";
+        String numberTest_2 = "//@,*\n2*3@009998";
         String numberTest_3 = "///\n0/75/5";
         String numberTest_4 = "//****\n1****0****1000";
         String numberTest_5 = "//*\n1*\n2*3";
         String numberTest_6 = "//%$@\n5%$@0%$@-1000";
+        String numberTest_7 = "//@,@@,@@@\n2@@\n3@@@8";
 
 
-        if (add(numberTest_1) != 1){
+        if (add(numberTest_1) != 3){
             System.out.println("The expected result was 1, instead it was "+ add(numberTest_1));
         }
         if (add(numberTest_2) != 5){
@@ -118,19 +151,27 @@ public class StringCalculator {
         if (add(numberTest_6) != 5){
             System.out.println("The expected result was 5, instead it was "+ add(numberTest_6));
         }
+        if (add(numberTest_7) != 13){
+            System.out.println("The expected result was 10, instead it was "+ add(numberTest_7));
+        }
 
         try {
             String negativeNumberTest1 = "//;\n-101;3;-4";
             add(negativeNumberTest1);
         }catch (Exception e){
-            System.out.println("Exception - "+ e + " - caught");
+            System.out.println("Exception test 1 - "+ e + " - caught");
         }
-
         try{
             String negativeNumberTest2 = "//@\n2@\n-3@8";
             add(negativeNumberTest2);
         }catch (Exception e){
-            System.out.println("Exception - "+ e + " - caught");
+            System.out.println("Exception test 2 - "+ e + " - caught");
+        }
+        try{
+            String negativeNumberTest3 = "//@,@@,@@@\n2@@\n-30@@@8";
+            add(negativeNumberTest3);
+        }catch (Exception e){
+            System.out.println("Exception test 3 - "+ e + " - caught");
         }
 
     }
