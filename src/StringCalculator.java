ff70ca55415e362c4e7a993019a17339197854c8
mad184 <mad184@usask.ca>
2021-01-25 19:09:49 -0600

    Set a description for function add() for Part_1

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,13 @@
 public class StringCalculator {
 
-    public static int add(){
+    /**
+     * A string calculator that receive as input a String with numbers separated
+     *   by a ",". The function add the numbers and return it as int.
+     *
+     * @param numbers - The String with numbers separated by ","
+     * @return the sum of the numbers
+     */
+    public static int add(String numbers){
         return 0;
     }
 
